bacbe0f6561ce95d3f40734ad351ec19d9e890ef
Paul <paulelliottwork@gmail.com>
2020-12-28 20:02:40 -0700

    initial stuff

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import TheNav from "./Components/Nav/theNav"
+
+function App() {
+  return (
+    <div className="App">
+      <TheNav></TheNav>
+      
+    </div>
+  );
+}
+
+export default App;
